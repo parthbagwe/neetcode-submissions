c7156dbef1a2db513d8e6df2d9b3be2b6a5a56b2
PPB <parthbagwee@gmail.com>
2026-09-03 00:22:34 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char, int> smap;
+        unordered_map<char, int> tmap;
+        for(int i=0; i<s.length(); i++){
+            smap[s[i]]++;
+            tmap[t[i]]++;
+        }
+        return smap==tmap;
+    }
+};
