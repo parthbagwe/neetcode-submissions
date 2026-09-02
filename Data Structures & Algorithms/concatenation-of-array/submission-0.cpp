d452d0e8229ff2c4d95d1d6c97f51f7c1103daea
PPB <parthbagwee@gmail.com>
2026-09-03 00:09:46 +0530

    Bulk sync: 1 submission

--- Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0; i<nums.size(); i++){
-            ans.push_back(nums[i]);
+        int size=nums.size()*2;
+        int size1=nums.size();
+        vector<int> res(size);
+        for(int i=0; i<size1; i++){
+            res[i]=nums[i];
         }
-        for(int i=0; i<nums.size(); i++){
-            ans.push_back(nums[i]);
+        for(int i=0; i<size1; i++){
+            res[i+size1]=nums[i];
         }
-        return ans;
+        return res;
     }
 };
